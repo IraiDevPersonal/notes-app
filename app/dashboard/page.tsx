c385224e7392d628e1e66859081c199de482a840
IraiDev <dev.iarriagada@gmail.com>
2025-10-27 19:17:23 -0300

    inicio de app

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function NotesPage() {
+  return (
+    <main className="p-4 grid place-content-center h-dvh space-y-4">
+      <h1 className="text-6xl font-bold">Mi espacio</h1>
+    </main>
+  );
+}
