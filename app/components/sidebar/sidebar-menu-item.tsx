f41e6955ce9225f5a98e46d1271fb04ae6deb457
iraidev <dev.iarriagada@gmail.com>
2025-12-21 18:30:30 -0300

    feat: Agrega componente SidebarMenuItem para elementos del menú lateral.

--- app/components/sidebar/sidebar-menu-item.tsx
+++ app/components/sidebar/sidebar-menu-item.tsx
@@ -10,7 +10,7 @@ type SidebarMenuItemProps = {
 const cls = {
   root: "relative overflow-hidden border border-transparent px-4 h-9 w-full flex items-center gap-2 hover:bg-foreground/10 rounded transition-colors outline-transparent ring-0 focus-visible:outline-ring focus-visible:outline-2 focus-visible:outline-offset-2",
   active:
-    'bg-foreground/10 after:content-[""] after:absolute after:w-2.5 after:h-full after:bg-foreground/30 after:left-0 border-foreground/30',
+    'bg-foreground/10 after:content-[""] after:absolute after:w-2.5 after:h-full after:bg-foreground/30 after:left-0 border-foreground/30 hover:bg-foreground/15',
 };
 
 export function SidebarMenuItem({ children, className }: SidebarMenuItemProps) {
