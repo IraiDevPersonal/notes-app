9da0e66cbec7f00b3e7d957e5a9f3b532209c113
iraidev <dev.iarriagada@gmail.com>
2025-12-21 18:32:29 -0300

    feat: Agrega el componente `Option` para elementos de selección.

--- app/components/select/option.tsx
+++ app/components/select/option.tsx
@@ -6,7 +6,7 @@ export function Option({ className, ...props }: OptionProps) {
   return (
     <option
       className={cn(
-        "flex justify-start items-center gap-2 p-2 transition-colors bg-input/50 text-foreground hover:bg-input hover:checked:bg-primary/80 checked:bg-primary text-sm [&>svg]:size-4 cursor-pointer",
+        "flex justify-start items-center gap-2 p-2 transition-colors bg-input/50 text-foreground hover:bg-input hover:checked:bg-primary/80 checked:bg-primary text-sm [&>svg]:size-4 cursor-pointer outline-none ring-0 focus-visible:bg-input",
         className
       )}
       {...props}
